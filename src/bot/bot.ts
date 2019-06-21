c32d01b2c53650bc54644faff30bb0814293fead
Briskicedtea28 <enausch@yahoo.com>
2019-06-21 01:53:55 -0700

    'Implemented order 66, sire'

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -12,8 +12,14 @@ client.on('ready', () => {
 });
 
 client.on('message', (msg) => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
+    if (msg.content === 'execute 66.exe') {
+        msg.channel.send('https://media.giphy.com/media/xTiIzrRyvrFijaEtY4/giphy.gif');
+        msg.channel.send('https://media.giphy.com/media/uGiQnHOGRvLpu/giphy.gif');
+        msg.channel.send('https://media.giphy.com/media/gr3rGWS6c7pKM/giphy.gif');
+        msg.channel.send('https://media.giphy.com/media/T4TCOdAe4OOUU/giphy.gif');
+        msg.channel.send('https://media.giphy.com/media/3r7yaG78CicpO/giphy.gif');
+        msg.channel.send('https://media.giphy.com/media/v7iNd2YXxb12E/giphy.gif');
+        msg.channel.send('https://media.giphy.com/media/3oKIPzLXQYb2Bn5PLG/giphy.gif');
     } else if (msg.content === "obiwan") {
         msg.channel.send('https://i.imgur.com/YBqg9JG.gifv');
     }
