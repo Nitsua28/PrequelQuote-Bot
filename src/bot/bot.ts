f16f0aba77d54c029e498340bef6379c6c7dfd25
Briskicedtea28 <enausch@yahoo.com>
2019-06-26 16:28:10 -0700

    "Added 'Quoteme' 30 quotes with images from quotes.txt"

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,11 +1,24 @@
 import { logger } from '../utils/logger';
-
+import { fstat } from 'fs';
 const { Client, MessageAttachment } = require('discord.js');
 const Auth = require('../../bot-auth.json');
-
 const client = new Client();
-
-
+const fs = require('fs'),
+    readline = require('readline'),
+    instream = fs.createReadStream('quotes.txt'),
+    outstream = new (require('stream'))(),
+    rl = readline.createInterface(instream, outstream);
+//created the map which has key:quote and value:image or gif
+let quotes = new Map();
+//new array to shove the keys in and iterate
+var temp = new Array(30);
+var count = 0;
+rl.on('line', function (line) {
+    var list = String(line).split("+");
+    quotes.set(list[0],list[1]);
+    temp[count] = list[0]; 
+    count++;
+});
 client.on('ready', () => {
     logger.debug(`Bot Ready and logged in as ${client.user.tag}!`);
     console.log(`Bot Online`);
@@ -25,6 +38,11 @@ client.on('message', (msg) => {
     }
     else if (msg.content === "YOU WERE THE CHOSEN ONE") {
         msg.channel.send('https://media.giphy.com/media/Mir5fnHxvXrTa/giphy.gif');
+    }
+    else if (msg.content === "quoteme") {
+        var randomQuote = temp[Math.floor(Math.random()* 30)];
+        msg.channel.send(randomQuote);
+        msg.channel.send(quotes.get(randomQuote));
     }
 });
 
