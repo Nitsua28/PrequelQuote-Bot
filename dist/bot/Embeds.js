9c24202f9312ad0c25cc06d1ecda9190656e2b55
Nitsua28 <achan113@ucmerced.edu>
2022-09-12 09:51:30 -0700

    hot fix 9/12

--- dist/bot/Embeds.js
+++ dist/bot/Embeds.js
@@ -5,9 +5,10 @@ const helpEmbed = new MessageEmbed()
   .setDescription('Welcome to Prequels Quote Generator!')
   .setThumbnail('https://cdn.discordapp.com/app-icons/591501223174209546/2d2e82027601f7ccd4005e07093e2f96.png?size=256')
   .addFields(
-        { name: '/random', value: 'Generates a random quote from the Prequels. You can sort by the two fields: Character and Movie.\n' },
+        { name: '/random', value: 'Generates a random quote from Star Wars! You can sort by Character, Movie or Trilogy.\n' },
         { name: '/help', value: 'Shows list of commands.\n'},
-        { name: '/meme', value: 'Choose your favorite meme from the Prequels.\n'}
+        { name: '/originaltrilogymemes', value: 'Choose your favorite meme from the Original Trilogy. \n'},
+        { name: '/prequelsmemes', value: 'Choose your favorite meme from the Prequels. \n'}
     )
   .setTimestamp();
 
