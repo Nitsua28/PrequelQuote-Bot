77bc325d60abb32202c4c8ad94e2c21cbfebe8f5
Austin Chan <briskicedtea28@austinDebian.local>
2026-08-18 20:53:22 -0700

    typo

--- src/bot/Embeds.js
+++ src/bot/Embeds.js
@@ -8,7 +8,7 @@ const helpEmbed = new EmbedBuilder()
   .setDescription('Welcome to Star Wars Quote Generator!')
   .setThumbnail('https://cdn.discordapp.com/app-icons/591501223174209546/2d2e82027601f7ccd4005e07093e2f96.png?size=256')
   .addFields(
-    { name: '/setautopostchannel', value: 'set a channel in your server to auto post a channel everyday at noon! (must have admin/mod privileges, and must be a text channel)\n' },
+    { name: '/setautopostchannel', value: 'set a channel in your server to auto post a quote everyday at noon! (must have admin/mod privileges, and must be a text channel)\n' },
     { name: '/searchquote', value: 'Search for your favorite quote from Star Wars!.\n' },
     { name: '/random', value: 'Generates a random quote from Star Wars! You can sort by Character, Movie, Trilogy or preDisney option.\n' },
     { name: '/help', value: 'Shows list of commands.\n' },
